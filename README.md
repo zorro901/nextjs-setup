5074fc7c506f3a91b8b2b6904f9157dce6af0f40
dl10yr <tkzw04202166@gmail.com>
2021-06-05 13:34:00 +0900

    adding comment and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# nextjs-setup
+This shell script automates the setup of Next.js + TypeScript + Tailwind + Storybook + eslint + prettier + husky project.
+detail: 
+
+# Usage
+```
+git clone https://github.com/dl10yr/nextjs-setup.git project_dir
+cd project_dir
+sh nextjs-setup.sh -n APP_NAME
+```
--- nextjs-setup.sh
+++ nextjs-setup.sh
@@ -16,7 +16,7 @@ fi
 echo "${APP_NAME}"
 npx create-next-app $APP_NAME
 cd $APP_NAME
-# yarn dev
+
 touch tsconfig.json
 
 yarn add --dev typescript @types/react @types/node
@@ -25,26 +25,17 @@ mv pages/index.js pages/index.tsx
 mv pages/api/hello.js pages/api/hello.tsx
 
 yarn add tailwindcss postcss autoprefixer
-
 npx tailwindcss init -p
-
-
-# tailwind.config.jsonのpurgeを置換
 sed -i '' "s/purge: \[\],/purge: \['\.\/pages\/\*\*\/\*\.tsx', '\.\/components\/\*\*\/\*\.tsx'\],/" tailwind.config.js
-
-# global.cssに追記
 mv ../globals.css ./styles/globals.css
 
 npx sb init
-
 yarn remove tailwindcss postcss autoprefixer
 yarn add tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
 mv stories components
 
 sed -i '' 's/\.\.\/stories\/\*\*\/\*\.stories\.mdx/\.\.\/components\/\*\*\/\*\.stories\.mdx/' .storybook/main.js
-
 sed -i '' 's/\.\.\/stories\/\*\*\/\*\.stories\.@(js|jsx|ts|tsx)/\.\.\/components\/\*\*\/\*\.stories\.@(js|jsx|ts|tsx)/' .storybook/main.js
-
 sed -i '' "1s/^/import \"..\/styles\/globals.css\"\n/" .storybook/preview.js
 
 cp ./components/Button.stories.tsx Button.stories.tsx
@@ -55,13 +46,12 @@ mv Button.stories.tsx ./components/Button.stories.tsx
 
 mv ../tailwind.config.js tailwind.config.js
 
+# prettier and eslint
 yarn add --dev eslint prettier eslint-plugin-react eslint-config-prettier eslint-plugin-prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin
-
 touch .eslintrc.json
 mv ../.eslintrc.json .eslintrc.json
 touch .eslintignore
 mv ../.eslintignore .eslintignore
-
 touch .prettierrc.json
 mv ../.prettierrc.json .prettierrc.json
 touch .prettierignore
@@ -70,12 +60,12 @@ mv ../.prettierignore .prettierignore
 mkdir .vscode
 mv ../settings.json ./.vscode/settings.json
 
+# install husky and lint-staged
 yarn add --dev husky lint-staged
 
+# adding script in package.json
 sed -i '' 's/"build-storybook": "build-storybook"/"build-storybook": "build-storybook",/' package.json
-
 LINE_NUMBER=`sed -n '/"build-storybook": "build-storybook",/=' package.json`
-
 LINE_NUMBER=`expr ${LINE_NUMBER} \+ 1`
 sed -i '' "${LINE_NUMBER}s/^/    \"lint\": \"eslint . --ext .ts,.js,.tsx,.jsx\",\n/" package.json
 LINE_NUMBER=`expr ${LINE_NUMBER} \+ 1`
@@ -85,13 +75,18 @@ sed -i '' "${LINE_NUMBER}s/^/    \"format\": \"prettier --write .\"\n/" package.
 LINE_NUMBER=`expr ${LINE_NUMBER} \+ 2`
 sed -i '' "${LINE_NUMBER}s/^/  \"lint-staged\": {\n    \"*.{js,jsx,ts,tsx}\": [\n      \"yarn lint\",\n      \"yarn format\"\n    ]\n  },\n/" package.json
 
+# remove .git/ of nextjs-setup.sh
 rm -rf ../.git
+
+# init git of project
 git init
 
+# setting up of husky
 npx husky init
 npx husky install
 npx husky add .husky/pre-commit "yarn lint-staged"
 
+# start vscode
 code .
 
 exit 1
